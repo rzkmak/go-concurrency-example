3877ee5fc7ecfe0351f4a773dd3718ec02d8a987
Rizki Maulana Akbar <rizkimaulanaakbar@outlook.com>
2019-09-04 16:53:43 +0700

    update checking pangram word

--- playground/string_pangram.go
+++ playground/string_pangram.go
@@ -24,9 +24,3 @@ func isPangram(word string) string {
 	}
 	return "pangram"
 }
-
-type RuneSlice []rune
-
-func (p RuneSlice) Len() int           { return len(p) }
-func (p RuneSlice) Less(i, j int) bool { return p[i] < p[j] }
-func (p RuneSlice) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
